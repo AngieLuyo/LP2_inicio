1a5c07678d63c784ec455e17378fc7222aad7188
angieluyo <luyoangi@gmail.com>
2020-06-10 18:22:45 -0500

    Modificando la conexion

--- src/util/MySqlDBConexion.java
+++ src/util/MySqlDBConexion.java
@@ -16,7 +16,7 @@ public class MySqlDBConexion {
 		Connection con=null;
 		try {
 			
-		    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/sedenorte?serverTimezone=CST","root","mysql");           
+		    con = DriverManager.getConnection("jdbc:mysql://node225993-env-2702977.j.layershift.co.uk/sedenorte?serverTimezone=CST","root","mysql");           
 		}catch (Exception e) {
 			e.printStackTrace();
 		}
